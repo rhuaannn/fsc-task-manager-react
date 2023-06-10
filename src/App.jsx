fdfc67066492d5d96c9997ca98b680729b5e549c
rhuaannn <rhuanmarques1223@gmail.com>
2023-06-10 12:03:37 -0300

    feat: components class reference

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import TaskItem from "./components/TaskItem";
-import "./App.css";
+ import TaskItem from "./components/TaskItem";
+ import "./App.css";
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -26,3 +26,34 @@ const App = () => {
 };
 
 export default App;
+
+// import React from "react";
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       tasks: [
+//         {
+//           id: "1",
+//           description: "	Estudar React ",
+//           isCompleted: false,
+//         },
+//         {
+//           id: "2",
+//           description: "Jogar free fire ",
+//           isCompleted: true,
+//         },
+//       ],
+//     };
+//   }
+//   render() {
+//     return (
+//       <>
+//           {this.state.tasks.map((task) => (
+//             <TaskItem key={task.id} task={task} />
+//           ))}
+//         </>
+//     )
+//   } 
+// }
+// export default App;
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,14 +1,26 @@
 import React from "react";
- 
-const TaskItem = ({task}) => {
- 
-  return(
+
+const TaskItem = ({ task }) => {
+  return (
     <>
-   <h2>
-   {task.description}
-    </h2> 
+      <h2>{task.description} {task.id}</h2>
+
     </>
   );
 };
 
 export default TaskItem;
+// class TaskItem extends React.Component {
+//   render() {
+//     const { task } = this.props;
+//     return (
+//     <>
+    
+//     <h1>{task.description}</h1>
+//     <p>{task.isCompleted ? "Completa" : "Não completa" }</p>
+//     </>
+//     );
+//   }
+// }
+// export default TaskItem;
+
