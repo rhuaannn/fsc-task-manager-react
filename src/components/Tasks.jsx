0b9e67f1188549ff0f82bf48e106b1cee48211c3
rhuaannn <rhuanmarques1223@gmail.com>
2023-06-11 19:03:22 -0300

    feat: fetchtasks after deletion

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -38,7 +38,7 @@ const Tasks = () => {
           {tasks
             .filter((task) => task.isCompleted === false)
             .map((lastTask) => (
-              <TaskItem task={lastTask} />
+              <TaskItem task={lastTask} fetchTasks={fetchTasks} />
             ))}
         </div>
       </div>
@@ -48,7 +48,7 @@ const Tasks = () => {
           {tasks
             .filter((task) => task.isCompleted === true)
             .map((completedTask) => (
-             <TaskItem task={completedTask} />
+             <TaskItem task={completedTask} fetchTasks={fetchTasks} />
             ))}
         </div>
       </div>
