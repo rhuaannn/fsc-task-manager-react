314ab43bc494872dd75816cfe3824bcc2d6a676d
rhuaannn <rhuanmarques1223@gmail.com>
2023-06-11 18:10:10 -0300

    add react icons faPlus

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,19 +1,26 @@
 import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
 
 import CustomInput from "./CustomInput";
+import CustomButton from "./CustomButton";
 import "./AddTask.scss";
 
 const AddTask = () => {
-  const [task, setTask] = useState('');
+  const [task, setTask] = useState("");
 
   const oonChange = (e) => {
     setTask(e.target.value);
   };
   return (
     <div className="add-task-container">
-      <CustomInput label="Adicionar Tarefa..." value={"task"}
+      <CustomInput
+        label="Adicionar Tarefa..."
+        value={"task"}
         onChange={oonChange}
       />
+      <CustomButton>
+        <FaPlus size={14} color="#ffffff" />
+      </CustomButton>
     </div>
   );
 };
