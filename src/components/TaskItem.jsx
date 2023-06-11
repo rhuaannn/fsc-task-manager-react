b4408cbf3cd8348ff2888a420b47eeede24b057e
rhuaannn <rhuanmarques1223@gmail.com>
2023-06-10 23:38:43 -0300

    feat: update taskItem componets

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,15 +1,29 @@
-import React  from "react";
+import React from "react";
+import "./TaskItem.scss"
 
+import {AiFillDelete} from "react-icons/ai";
 const TaskItem = ({ task }) => {
- 
-
   return (
-    <React.Fragment key={task.id}>
-      <h1>
-        {task.description} {task.id}
-      </h1>
-      <h3>{task.isCompleted ? "Completa" : "nao completa"}</h3>
-    </React.Fragment>
+    <div className="task-item-container">
+      <div className="task-description">
+        <label
+          className={
+            task.isCompleted
+              ? "checkbox-container-completed"
+              : "checkbox-container"
+          }
+        >
+          {task.description}
+          <input type="checkbox" defaultChecked={task.isCompleted} />
+          <span
+            className={task.isCompleted ? "checkmark completed" : "checkmark"}
+          ></span>
+        </label>
+      </div>
+      <div className="delete">
+          <AiFillDelete size={18} color="#f97474"/>
+      </div>
+    </div>
   );
 };
 
